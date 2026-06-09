33472a872997922c7f5d396ea2e4734efca2f89e
Rajesh Parajuli <rajeshparajuli146@gmail.com>
2026-06-09 15:18:50 +0545

    first commit

---
+++ Hrs.Api.Repository/Data/HrsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hrs.Api.Repository.Data
+{
+    public class HrsDbContext : DbContext
+    {
+        public HrsDbContext(DbContextOptions<HrsDbContext> options)
+            : base(options)
+        {
+        }
+        //public DbSet<Author> Author { get; set; }
+    }
+}
